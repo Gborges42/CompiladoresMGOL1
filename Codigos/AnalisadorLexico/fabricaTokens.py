0a6be0e719c8ecb49faf122c01175a61d1393d88
Gborges42 <45388282+Gborges42@users.noreply.github.com>
2022-07-04 21:37:56 -0300

    Update fabricaTokens.py

--- Codigos/AnalisadorLexico/fabricaTokens.py
+++ Codigos/AnalisadorLexico/fabricaTokens.py
@@ -18,7 +18,7 @@ tokens = {
 }
 
 class Token:
-    def __init__(self, lexema, classe, tipo):
+    def __init__(self, lexema, estado):
         self.tokens = {
             1: "Num", 
             3: "Num",
@@ -38,5 +38,15 @@ class Token:
             21: "VIR"
         }
         self.lexema = lexema
-        self.classe = self.tokens[classe]
-        self.tipo = tipo
+        self.classe = self.tokens[estado]
+        if estado == 1:
+            self.tipo = "inteiro"
+        elif (estado == 3)or(estado == 6):
+            self.tipo = "real"
+        elif estado == 8:
+            self.tipo = "literal"
+        else: 
+            self.tipo = "NULO"
+
+    def __repr__(self):
+        return f'Token(classe:{self.classe}, lexema:{self.lexema}, tipo:{self.tipo})'
